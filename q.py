2eb3bab4f5423487e94736e89636bbbc4de1aa6b
frisko13 <147921173+frisko13@users.noreply.github.com>
2023-10-27 17:46:53 +0300

    Update q.py

--- q.py
+++ q.py
@@ -1,3 +1,22 @@
+
+names = {"Андрей": 25, "Мария": 30, "Иван": 20, "Наталья": 35, "Петр": 27}
+
+while True:
+    name = input("Введите имя человека: ")
+    if name in names:
+        print(f"Возраст {name}: {names[name]} лет")
+    else:
+        print("НЕТУ ТАКОГО")
+
+
+
+
+
+
+
+
+
+
 def count_zeros_in_factorial(n):
     count = 0
     i = 5
